6b2f110c80d8dcbaaac3ca262b87d54062a63268
Tabisz <maro.990@gmail.com>
2018-05-03 12:26:20 +0200

    Update

--- Inventory/Assets/Scripts/Inventory.cs
+++ Inventory/Assets/Scripts/Inventory.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
+
+
+    public Canvas canvas;
+
+    private float hoverYOffset;
 
     private RectTransform inventoryRect;
 
@@ -16,25 +23,90 @@ public class Inventory : MonoBehaviour {
 
     public float slotSize;
 
+    public EventSystem eventSystem;
+
     public GameObject slotPrefab;
 
-    private List<GameObject> allSlots; 
+    private static Slot from, to;
+
+    private List<GameObject> allSlots;
+
+    public GameObject iconPrefab;
+
+    private static GameObject hoverObject;
+
+    private static int emptySlots;
+
+    public static int EmptySlots            //po co to jak mozna zmienic private na public?
+    {
+        get {return emptySlots;}
+        set {emptySlots = value;}
+    }
+
+    public CanvasGroup canvasGroup;
+
+    private bool fadingIn;
+
+    private bool fadingOut;
+
+    public float fadingTime;
 
     void Start()
     {
         CreateLayout();
+
     }
 
 
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(!eventSystem.IsPointerOverGameObject(-1)&& from != null)
+            {
+                from.GetComponent<Image>().color = Color.white;
+                from.ClearSlot();
+                Destroy(hoverObject);
+                to = null;
+                from = null;
+                hoverObject = null;
+                emptySlots++;
+
+            }
+        }
+		if(hoverObject !=null)
+        {
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out position);
+            position.Set(position.x, position.y - hoverYOffset);
+            hoverObject.transform.position = canvas.transform.TransformPoint(position);
+        }
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+
+            if(canvasGroup.alpha>0)
+            {
+                StartCoroutine("FadeOut");
+            }
+            else
+            {
+
+
+                StartCoroutine("FadeIn");
+            }
+        }
 	}
 
     private void CreateLayout()
     {
         allSlots = new List<GameObject>();
+
+        hoverYOffset = slotSize * 0.02f;
+
+        emptySlots = slots;
         int columns = slots / rows;
         inventoryWidth = (columns) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
         inventoryHeight = rows * (slotSize + slotPaddingTop) + slotPaddingTop;
@@ -57,11 +129,159 @@ public class Inventory : MonoBehaviour {
 
                 slotRect.localPosition = inventoryRect.localPosition + new Vector3(slotPaddingLeft * (x + 1) + (slotSize * x), -slotPaddingTop * (y + 1) - (slotSize * y));
 
-                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
-                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize * canvas.scaleFactor);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize * canvas.scaleFactor);
 
                 allSlots.Add(newSlot);
             }
+        }
+    }
+
+    public bool AddItem(Item item)
+    {
+        if (item.maxSize == 1)
+        {
+            PlaceEmpty(item);
+            return true;
+        }
+        else
+        {
+            foreach (GameObject slot in allSlots)
+            {
+                Slot tmp = slot.GetComponent<Slot>();
+                if (!tmp.IsEmpty)
+                {
+                    if(tmp.CurrentItem.type == item.type && tmp.IsAvailable)
+                    {
+                        tmp.AddItem(item);
+                        return true;
+                    }
+                }
+            }
+        }
+        if (emptySlots > 0)
+        {
+            PlaceEmpty(item);
+        }
+        return false;
+    }
+
+    private bool PlaceEmpty(Item item)
+    {
+        if(emptySlots>0)
+        {
+            foreach (GameObject slot in allSlots)
+            {
+                Slot tmp = slot.GetComponent<Slot>();
+                if(tmp.IsEmpty)
+                {
+                    tmp.AddItem(item);
+                    emptySlots--;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void MoveItem(GameObject clicked)
+    {
+        if(from == null)
+        {
+            if(!clicked.GetComponent<Slot>().IsEmpty)
+            {
+                from = clicked.GetComponent<Slot>();
+                from.GetComponent<Image>().color = Color.gray;
+
+                hoverObject =Instantiate(iconPrefab);
+                hoverObject.GetComponent<Image>().sprite = clicked.GetComponent<Image>().sprite;
+                hoverObject.name = "Hover";
+
+                RectTransform hoverTransform = hoverObject.GetComponent<RectTransform>();
+                RectTransform clickedTransform = clicked.GetComponent<RectTransform>();
+
+                hoverTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, clickedTransform.sizeDelta.x);
+                hoverTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, clickedTransform.sizeDelta.y);
+
+                hoverObject.transform.SetParent(GameObject.Find("Canvas").transform, true);
+                hoverObject.transform.localScale = from.gameObject.transform.localScale;
+            }
+        }
+        else if(to == null)
+        {
+            to = clicked.GetComponent<Slot>();
+            Destroy(hoverObject);
+        }
+        if(to != null && from !=null)
+        {
+            Stack<Item> tmpTo = new Stack<Item>(to.Items);
+            to.AddItems(from.Items);
+
+            if (tmpTo.Count == 0)
+            {
+                from.ClearSlot();
+            }
+            else
+            {
+                from.AddItems(tmpTo);
+            }
+            from.GetComponent<Image>().color = Color.white;
+            to = null;
+            from = null;
+            hoverObject = null;
+        }
+    }
+
+
+    private IEnumerator FadeOut()
+    {
+        Debug.Log("ddd");
+        if (!fadingOut)
+        {
+            fadingOut = true;
+            fadingIn = false;
+            StopCoroutine("FadeIn");
+
+            float startAlpha = canvasGroup.alpha;
+            float progress = 0.0f;
+            float rate = 1.0f / fadingTime;
+
+            while (progress<1.0)
+            {
+                canvasGroup.alpha = Mathf.Lerp(startAlpha, 0, progress);
+                progress += rate * Time.deltaTime;
+
+                yield return null;
+            }
+
+            canvasGroup.alpha = 0;
+            fadingOut = false;
+
+        }
+    }
+    private IEnumerator FadeIn()
+    {
+        if (!fadingIn)
+        {
+            fadingIn = true;
+            fadingOut = false;
+            StopCoroutine("FadeOut");
+
+            float startAlpha = canvasGroup.alpha;
+            float progress = 0.0f;
+            float rate = 1.0f / fadingTime;
+
+            while (progress < 1.0)
+            {
+                canvasGroup.alpha = Mathf.Lerp(startAlpha, 1, progress);
+                progress += rate * Time.deltaTime;
+
+                yield return null;
+            }
+
+            canvasGroup.alpha = 1;
+            fadingIn = false;
+
         }
     }
 }
---
+++ Inventory/Assets/Scripts/Slot.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IPointerClickHandler  {
+
+    private Stack<Item> items;
+    public Text stackTxt;
+
+    public Sprite slotEmpty;
+    public Sprite slotHiglight;
+
+    public bool IsEmpty
+    {
+        get { return items.Count == 0; }
+    }
+
+    public Item CurrentItem
+    {
+        get { return items.Peek(); }
+    }
+    public bool IsAvailable
+    {
+        get { return CurrentItem.maxSize > items.Count; }
+    }
+
+    public Stack<Item> Items
+    {
+        get { return items;}
+        set { items = value; }
+    }
+
+    // Use this for initialization
+    void Start () {
+        items = new Stack<Item>();
+        RectTransform slotRect = GetComponent<RectTransform>();
+        RectTransform txtRect = stackTxt.GetComponent<RectTransform>();
+
+        int txtScaleFactor = (int)(slotRect.sizeDelta.x * 0.60f);
+        stackTxt.resizeTextMinSize = txtScaleFactor;
+        stackTxt.resizeTextMaxSize = txtScaleFactor;
+        txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotRect.sizeDelta.x);
+        txtRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotRect.sizeDelta.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void AddItem(Item item)
+    {
+        items.Push(item);
+        if (items.Count > 1)
+            stackTxt.text = items.Count.ToString();
+
+        ChangeSprite(item.spriteNeutral, item.spriteHighlighted);
+    }
+
+    public void AddItems(Stack<Item> items)
+    {
+        this.items = new Stack<Item>(items);
+
+        stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty;
+
+        ChangeSprite(CurrentItem.spriteNeutral, CurrentItem.spriteHighlighted);
+    }
+
+    private void ChangeSprite(Sprite neutral, Sprite highlight)
+    {
+        GetComponent<Image>().sprite = neutral;
+        SpriteState st = new SpriteState();
+        st.highlightedSprite = highlight;
+
+        GetComponent<Button>().spriteState = st;
+    }
+
+    private void UseItem()
+    {
+        if(!IsEmpty)
+        {
+            items.Pop().Use();
+
+            stackTxt.text = items.Count > 1 ? items.Count.ToString() : string.Empty;    //dwa razy sprawdza czy slot jest pusty, chyba da sie to lepiej zrobic
+
+            if(IsEmpty)
+            {
+                ChangeSprite(slotEmpty, slotHiglight);
+
+                Inventory.EmptySlots++;
+            }
+        }
+    }
+
+    public void ClearSlot()
+    {
+        items.Clear();
+        ChangeSprite(slotEmpty, slotHiglight);
+        stackTxt.text = string.Empty;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right && !GameObject.Find("Hover"))
+        {
+            UseItem();
+        }
+    }
+}
